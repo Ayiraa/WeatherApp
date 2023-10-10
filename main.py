ee10df80cda028a44571ce7ab5756f343e0e78f1
Karolina <kkitta@spoko.pl>
2023-10-10 17:15:08 +0200

    db added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QComboBox, QCompleter, QLineEdit
+from PyQt5.QtCore import Qt
 from PyQt5.uic import loadUi
 import sys
 import requests
-import json
-import time
+import sqlite3
 
 api_key = '960001ee3cd72d2a0e47e49f2c1c13f5'
 
@@ -19,14 +19,19 @@ class WeatherApp(QMainWindow):
         self.cityLineEdit = QLineEdit(self)
         self.cityComboBox.setLineEdit(self.cityLineEdit)
 
-        self.city_data = self.load_or_retrieve_city_data()
+        # Initialize SQLite database and load city data
+        self.conn = sqlite3.connect("weather.db")
+        self.cursor = self.conn.cursor()
 
-        # Extract city names from the data
-        city_names = [f"{city['name']}, {city['country']}" for city in self.city_data]
+        # Retrieve city names and country codes directly from the database
+        self.city_data = self.retrieve_city_data()
+
+        # Extract city names and format them as "City, Country"
+        city_names = [f"{name}, {country}" for name, country in self.city_data]
 
         # Create a QCompleter and associate it with the QLineEdit
         completer = QCompleter(city_names, self)
-        completer.setCaseSensitivity(False)  # Make suggestions case-insensitive
+        completer.setCaseSensitivity(Qt.CaseInsensitive)  # Make suggestions case-insensitive
         self.cityLineEdit.setCompleter(completer)
 
         # Assign data to the QComboBox
@@ -34,7 +39,6 @@ class WeatherApp(QMainWindow):
 
         # Connect the button click event to a function
         self.Check_button.clicked.connect(self.check_city_weather)
-
 
     def check_city_weather(self):
         # Get the selected city from the QComboBox
@@ -54,37 +58,13 @@ class WeatherApp(QMainWindow):
         else:
             QMessageBox.warning(self, "Error", "Please select a city from the list.")
 
-    def load_or_retrieve_city_data(self):
-        # Cache expiration time (in seconds)
-        CACHE_EXPIRATION = 3600  # 1 hour
-
-        # Cache city data in memory if it's not already cached
-        current_time = int(time.time())
-        if (
-                not hasattr(self, "city_data_cache")
-                or current_time - self.city_data_cache.get("timestamp", 0) > CACHE_EXPIRATION
-        ):
-            json_file_path = 'staticresources/city_data.json'
-
-            try:
-                with open(json_file_path, 'r', encoding='utf-8') as json_file:
-                    city_data = json.load(json_file)
-                # Cache the data
-                self.city_data_cache = {"data": city_data, "timestamp": current_time}
-            except Exception as e:
-                # Handle the exception (e.g., show an error message)
-                print(f"Error loading city data: {str(e)}")
-                # Use the previously cached data if available
-                city_data = self.city_data_cache.get("data", [])
-        else:
-            # Use the cached data
-            city_data = self.city_data_cache.get("data", [])
-
-        return city_data
+    def retrieve_city_data(self):
+        # Retrieve city names and country codes from the database
+        self.cursor.execute("SELECT name, country FROM cities")
+        return self.cursor.fetchall()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = WeatherApp()
     window.show()
     app.exec_()
-
